e2429420616256616a8a1b2fca6de172548d0f88
Amal-els <amalbahri19@gmail.com>
2025-05-04 19:59:32 +0100

    adding Commande entity

---
+++ src/Entity/Commande.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $prix_total = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrixTotal(): ?float
+    {
+        return $this->prix_total;
+    }
+
+    public function setPrixTotal(float $prix_total): static
+    {
+        $this->prix_total = $prix_total;
+
+        return $this;
+    }
+}
