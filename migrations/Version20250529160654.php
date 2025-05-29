2bbca50c5f8ec06ce6685f56718b2b157d89e857
tasnimchaouch0 <tasnim.chaouch@insat.ucar.tn>
2025-05-29 18:04:44 +0100

    added quantité attribute

---
+++ migrations/Version20250529160654.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250529160654 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE commande ADD date_commande DATE NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD quantité VARCHAR(55) DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE commande DROP date_commande
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product DROP quantité
+        SQL);
+    }
+}
